d50e0e56a2cbf94c70e5b1bd795427fef71db4e4
Mirelly Almeida <136247614+Mihvieira@users.noreply.github.com>
2024-05-05 11:16:18 -0400

    tabs

--- assets/js/poke-detalhes.js
+++ assets/js/poke-detalhes.js
@@ -1,7 +1,8 @@
-const element = document.getElementsByClassName("interface")
 const params = new URLSearchParams(window.location.search)
 const id = params.get("id")
-const heart = document.querySelector("heart")
+const heart = document.querySelector(".heart")
+const tabs = document.querySelectorAll(".botao_detalhe")
+
 
 function transformData(pokeDetail) {
     const pokemon = new Pokemon()
@@ -43,9 +44,9 @@ function transformData(pokeDetail) {
     return pokemon
 }
 
-function convertPokemonToHtml(pokemon){
+function convertPokemonToHtml(infos){
     const newHtml =  document.write(`
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
@@ -74,41 +75,41 @@ function convertPokemonToHtml(pokemon){
 </head>
 <body>
     <div id="detailPokemon"class="interface">
-    <div class="pokemon">
-    <div class="icons">
-        <a href="index.html"><i class="fa-solid fa-arrow-left"></i></a>
-        <button class="heart"><i class="fa-regular fa-heart"></i></button>
-    </div>
-    <div class="basicInfo">
-            <span class="name">${pokemon.name}</span>
-            <span class="number">#${pokemon.number}</span>
-        <div class="detail">
-        <ol class="types">
-                                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+        <div class="pokemon">
+            <div class="icons">
+                <a href="index.html"><i class="fa-solid fa-arrow-left"></i></a>
+                <button class="heart"><i class="fa-regular fa-heart"></i></button>
+            </div>
+            <div class="basicInfo">
+                <span class="name">${infos.name}</span>
+                <span class="number">#${infos.number}</span>
+                <div class="detail">
+                    <ol class="types">
+                                ${infos.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                             </ol>
-    </div>
-</div> <!--pokemon-->
+                </div>
+        </div> <!--pokemon-->
 
-<div class="imgPokemon">
-    <img src="${pokemon.photo}" alt="${pokemon.name}">
-</div>
+        <div class="imgPokemon">
+        <img src="${infos.photo}" alt="${infos.name}">
+        </div>
 
-<div class="pagination_details">
-    <div class="tab_links">
-            <button id="defaultOpen" class="botao_detalhe" name="about">About</button>
-            <button class="botao_detalhe" name="base-stats")">Base Stats</button>
-            <button class="botao_detalhe" name="moves")">Moves</button>
-    </div><!--tab_links-->
-        <div id="about" class="subdetails">
+    <div class="pagination_details">
+        <div class="tab_links">
+            <button  class="botao_detalhe active" content-id="about">About</button>
+            <button class="botao_detalhe" content-id="status")">Base Stats</button>
+            <button class="botao_detalhe" content-id="moves")">Moves</button>
+        </div><!--tab_links-->
+        <div  class="subdetails">
                 <div class="types">
-                    <ol class="atributos">
+                    <ol id="about"class="atributos">
                         <li class="atributo">
                             <h3>heght</h3>
-                            <p>${pokemon.height}</p>
+                            <p>${infos.height}</p>
                             <h3>weight</h3>
-                            <p>${pokemon.weight}</p>
+                            <p>${infos.weight}</p>
                             <h3>abilities</h3>
-                            <li>${pokemon.abilities.map((ability) => `<li class="ability ${ability}">${ability}</li>`).join('')}</li>
+                            <li>${infos.abilities.map((ability) => `<li class="ability ${ability}">${ability}</li>`).join('')}</li>
                             <h2>Breeding</h2>
                             <p></p>
                         </li>
@@ -116,30 +117,30 @@ function convertPokemonToHtml(pokemon){
                 </div><!--types-->
         </div><!--subdetails-->
 
-        <div id="status" class="subdetails">
+        <div class="subdetails">
             <div class="types">
-                <ol class="atributos">
+                <ol id="status" class="atributos" >
                     <li class="atributo sts">
-                    ${pokemon.stats.map((stat) => `<h3 class="stat ${stat}">${stat}</h3>`).join('')}
-                        <p>${pokemon.statsNumber.map((statNumber) => `<h3 class="stat ${statNumber}">${statNumber}</p>`).join('')}</p>
+                    ${infos.stats.map((stat) => `<h3 class="stat ${stat}">${stat}</h3>`).join('')}
+                        <p>${infos.statsNumber.map((statNumber) => `<h3 class="stat ${statNumber}">${statNumber}</p>`).join('')}</p>
                         <div class="progress-status"> 
                             <div class="progressbar">
                             </li>
                         </div>
                 </ol><!--atributos-->
             </div><!--types-->
-    </div><!--subdetails-->
+        </div><!--subdetails-->
 
-    <div id="moves" class="subdetails">
-    <div class="types">
-        <ol class="atributos">
-            <li class="atributo move">
-            <p>${pokemon.moves.map((move) => `<h3 class="move ${move}">${move}</h3>`).join('')}</p>
-            </li>
-        </ol><!--atributos-->
-    </div><!--types-->
-</div><!--subdetails-->
-</div><!--pagination_details-->
+        <div  class="subdetails">
+            <div class="types">
+                <ol id="moves" class="atributos">
+                    <li class="atributo move">
+                    <p>${infos.moves.map((move) => `<h3 class="move ${move}">${move}</h3>`).join('')}</p>
+                    </li>
+                </ol><!--atributos-->
+            </div><!--types-->
+        </div><!--subdetails-->
+    </div><!--pagination_details-->
 </div>
 </body>
 </html>
@@ -167,3 +168,21 @@ heart.addEventListener("click", ()=>{
     iconHeart.style.color = "red";
     return iconHeart
 })
+
+tabs.forEach(tab => tab.addEventListener("click", ()=> tabClicked(tab)))
+
+const tabClicked = (tab)=> {
+    tabs.forEach(tab => tab.classList.remove("active"))
+    tab.classList.add("active")
+
+    const contents = document.querySelectorAll(".subdetails")
+    contents.forEach(content => content.classList.remove("show"))
+
+    const contentId = tab.getAttribute("content-id")
+    const content = document.getElementById(contentId)
+
+    content.classList.add("show")
+}
+
+const currentActiveTab = document.querySelector('.botao_detalhe.active')
+tabClicked(currentActiveTab)
